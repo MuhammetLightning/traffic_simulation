7e255ad14b5d6e8336c0e4f28f7a305483776e9c
MuhammetLightning <muhammet.lightning@gmail.com>
2025-04-18 16:55:58 +0300

    Port ayarı güncellendi

--- server.py
+++ server.py
@@ -40,6 +40,4 @@ def health_check():
     })
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    debug = os.getenv('FLASK_ENV') == 'development'
-    app.run(host='0.0.0.0', port=port, debug=debug) 
+    app.run(host='0.0.0.0', port=5000, debug=False) 
